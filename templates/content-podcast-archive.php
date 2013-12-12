3c2c1eab5ee7f5752f6efefb8193623a155374c5
Kelly Anne Pipe <kpipe@sju.edu>
2013-12-12 16:04:55 -0500

    Carousel

--- templates/content-podcast-archive.php
+++ templates/content-podcast-archive.php
@@ -13,8 +13,8 @@
 				<?php if(get_field('segment')): ?>
 				<h3>Segments</h3>
 				<?php while(has_sub_field('segment')): ?>
-				<?php $term = get_term_by('id',get_sub_field('segment_type'),'segmenttax'); ?>
-				<img class="segment-icon" data-toggle="tooltip" title="<?php the_sub_field('segment_description'); ?>" src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/icons/<?php echo($term->slug); ?>.png">
+				  <?php $term = get_term_by('id',get_sub_field('segment_type'),'segmenttax'); ?>
+          <img class="segment-icon" src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/icons/<?php echo($term->slug); ?>.png">
 		 
 				<?php endwhile;endif; ?>
 			</div>
