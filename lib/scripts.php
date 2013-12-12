1bd08646c3081e4adb007445b8cd5c1bd8f7dfe8
Kelly Anne Pipe <kpipe@sju.edu>
2013-12-12 16:24:57 -0500

    Carousel

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1714d89efe0e4cfbb7bd956315f7669d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e8542c817b2522f12d89c79e5490f267');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
