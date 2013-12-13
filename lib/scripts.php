34a94731e5c968e22c7eb8781ea582d3868897ff
Kelly Anne Pipe <kpipe@sju.edu>
2013-12-13 12:38:59 -0500

    Front Page

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e367eaf848fd0c10b2ea9f9e7cfd64ea');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '067ff9cebc819bb24b3d53d514beb0de');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
