211c5d02e8e01fdbfdf6802a4c0ff52d338f950c
Kelly Anne Pipe <kpipe@sju.edu>
2013-12-13 15:27:43 -0500

    Correcting the error for the path

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '29eccd49e64b29c0413955299a41377d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'caf07d17467e1ef01f1b8da59829de67');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
