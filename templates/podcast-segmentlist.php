0ebd64bb987ce1afc2c44e245afe8eebd2feb4bd
Kelly Anne Pipe <kpipe@sju.edu>
2013-12-13 12:34:35 -0500

    Front Page

--- templates/podcast-segmentlist.php
+++ templates/podcast-segmentlist.php
@@ -1,6 +1,6 @@
 <?php while(has_sub_field('segment')): ?>
 <?php $term = get_term_by('id',get_sub_field('segment_type'),'segmenttax'); ?>
-  <i class="segment-icon-fa <?php echo $term?>"></i>
-  <img class="segment-icon <?php echo $term?>" data-toggle="tooltip" title="<?php the_sub_field('segment_description'); ?>" src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/icons/<?php echo($term->slug); ?>.png">
+  <i class="segment-icon-fa <?php echo($term->slug); ?>"></i>
+  <img class="segment-icon <?php echo($term->slug); ?>" data-toggle="tooltip" title="<?php the_sub_field('segment_description'); ?>" src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/icons/<?php echo($term->slug); ?>.png">
 
 <?php endwhile; ?>
