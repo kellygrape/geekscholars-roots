a781f64367c1e7a2f9553b48f73503007e53a3c0
Kelly Anne Pipe <kpipe@sju.edu>
2013-12-16 10:08:34 -0500

    Trying to get the main logo on inner pages working.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a867521f56a87ef9f9727ba6f11ac191');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b495a2aba3c01313d1bd9865ed5f00e8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
