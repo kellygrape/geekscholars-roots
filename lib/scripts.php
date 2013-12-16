5a5f9abdccd3c9709651c014e786754f2ee5dc05
Kelly Anne Pipe <kpipe@sju.edu>
2013-12-16 10:33:14 -0500

    Slider meta

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f29d971d56a02c3168e14af9f2d9c6c2');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '470801fdfb9073108367d875fa380401');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
