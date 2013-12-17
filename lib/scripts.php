e3b4f2c202f49e2a3416b06e8d0acfd0f328612e
Kelly Anne Pipe <kpipe@sju.edu>
2013-12-17 16:32:29 -0500

    Navbar

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'be0864ea1f311340c09858e54c4b9ae0');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b4172cc2a31eba09c159a6cd99cd4163');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
