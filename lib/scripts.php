79b684563244a804063ee005d36185ac2ffadfe1
Kelly Anne Pipe <kpipe@sju.edu>
2013-12-17 16:33:35 -0500

    Segment Icons

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b4172cc2a31eba09c159a6cd99cd4163');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ce8fd03ac3153fe532d7118b8d5eadee');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
