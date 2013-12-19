321ca01a7c5a6d87a35754e3b0412f8a4129920d
Kelly Anne Pipe <kellyanne.martin@gmail.com>
2013-12-19 12:31:40 -0500

    Add the new template.

---
+++ template-moviereviews.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Movie Reviews Listing Template
+*/
+?>
+
+<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/content', 'page'); ?>
+
+
+<?php 
+  $ratings = get_terms('ratingstax');
+    foreach($ratings as $rating) {
+      $ratedposts = get_posts(array( 'taxonomy' => 'ratingstax', 'term' => $rating->slug, 'orderby' => 'date', 'order' => 'DESC'));
+      ?>
+      <h2 class="movie-review-rating <?php echo $rating->slug; ?>"><?php echo $rating->name; ?></h2>
+      <ul class="ratings-post-list">
+      <?php foreach ( $ratedposts as $post ) : setup_postdata( $post ); ?>
+        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+
+      <?php endforeach; wp_reset_postdata();?>
+      </ul>
+    <?php
+    }
+    ?>
