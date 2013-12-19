3272d08d39650492749b994a74b8ce446bbe0825
Kelly Anne Pipe <kellyanne.martin@gmail.com>
2013-12-19 10:22:33 -0500

    Playing with colors

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '188e40132ecc0f6579ead655150c62eb');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9ec314e9ca2772207bc93245f16ebc00');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
