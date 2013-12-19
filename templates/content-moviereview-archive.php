9dbd28737c79c1ace420bdc49d2c8d84682a2ee7
Kelly Anne Pipe <kpipe@sju.edu>
2013-12-19 09:20:55 -0500

    Movie Reviews Test

--- templates/content-moviereview-archive.php
+++ templates/content-moviereview-archive.php
@@ -1,9 +1,14 @@
-<article <?php post_class(); ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
-  </header>
-  <div class="entry-summary">
-    <?php the_content('Read more...'); ?>
-  </div>
+<article <?php post_class('row'); ?>>
+  <figure class="movie-review-poster col-md-3">
+    <?php the_post_thumbnail(); ?>
+  </figure>
+  <section class="movie-review-snippet col-md-9">
+    <header>
+      <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <?php get_template_part('templates/entry-meta'); ?>
+    </header>
+    <div class="entry-summary">
+      <?php the_content('Read more...'); ?>
+    </div>
+  </section>
 </article>
