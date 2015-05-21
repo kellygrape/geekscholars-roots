1c41fa834eeeb2b615f36a5bd4802bf3f05b0d56
Kelly Anne <geeksch1@box777.bluehost.com>
2015-05-20 18:05:13 -0600

    footer stuff

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,26 @@
 <footer class="content-info" role="contentinfo">
   <section class="container">
   <div class="row">
-    <div class="col-lg-12">
+    <div class="col-sm-4 col-md-3">
+      <nav class="social-area">
+        <a href="https://www.facebook.com/geekscholars" title="Visit us on Facebook" class="btn">
+          <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/facebook.png" alt="Facebook">
+        </a>
+        <a href="https://twitter.com/GeekScholars" title="Follow us on Twitter" class="btn">
+          <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/twitter.png" alt="twitter">
+        </a>
+        <a href="https://itunes.apple.com/us/podcast/geekscholars-movie-news/id459567560" title="Visit GeekScholars on iTunes" class="btn">
+          <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/itunes.png" alt="Visit GeekScholars on iTunes">
+        </a>
+        <a href="mailto:mail@geekscholars.com" title="Email GeekScholars" class="btn">
+          <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/email.png" alt="Email GeekScholars">
+        </a>
+        <a href="http://www.geekscholars.com/feed/podcast/" title="Subscribe to Podcast on Android" class="btn">
+          <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/android.png" alt="Subscribe to Podcast on Android">
+        </a>
+      </nav>
+    </div>
+    <div class="col-sm-8 col-md-9">
       <?php dynamic_sidebar('sidebar-footer'); ?>
       <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
     </div>
